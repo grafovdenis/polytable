6eede856d381eb8b201ae3715bbe7923381fc20f
grafa <grafov.denis@gmail.com>
2018-10-28 17:12:29 +0300

    prettier main page

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,12 @@ import 'dart:convert';
 
 import 'package:polytable/Constants.dart';
 import 'package:polytable/Group.dart';
-import 'package:polytable/templates/Header.dart';
 import 'package:polytable/templates/SearchResult.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  //TODO fix navigation
+  //TODO fix navigation, divide into classes
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -35,11 +34,20 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
+//Убираем эффект от скролла
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   void choiceAction(String choice) {
     if (choice == Constants.Group) {
       Navigator.push(context,
-          MaterialPageRoute(builder: (context) => new Group(name: "33531/1")));
+          MaterialPageRoute(builder: (context) => new Group(name: "33531/2")));
     }
   }
 
@@ -78,57 +86,87 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Color.fromRGBO(16, 93, 59, 1.0),
-      appBar: Header(
-        choiceAction: choiceAction,
-        elevation: 0.0,
-      ),
-      body: new ListView(
-        children: <Widget>[
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 80.0, bottom: 30.0),
-              child: Column(
-                children: <Widget>[
-                  InkWell(
-                    child: Image(
-                      image: new AssetImage('assets/images/biglogo.png'),
-                      width: 300.0,
+    final phrase = new Text(phrases[new Random().nextInt(phrases.length)],
+        style: TextStyle(color: Colors.white, fontSize: 18.0));
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              elevation: 0.0,
+              actions: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: new PopupMenuButton(
+                      icon: Icon(Icons.account_circle, size: 40.0),
+                      onSelected: choiceAction,
+                      itemBuilder: (BuildContext context) {
+                        return Constants.choices.map((String choice) {
+                          return PopupMenuItem<String>(
+                            value: choice,
+                            child: Text(choice),
+                          );
+                        }).toList();
+                      },
+                    ))
+              ],
+              backgroundColor: Color.fromRGBO(16, 93, 59, 1.0),
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Image(
+                  image: new AssetImage('assets/images/biglogo.png'),
+                  width: 200.0,
+                ),
+                background: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0),
+                      child: phrase,
                     ),
-                    onTap: () {
-                      setState(() {});
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: new Text(
-                        phrases[new Random().nextInt(phrases.length)],
-                        style: TextStyle(color: Colors.white, fontSize: 18.0)),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: new TextField(
-              onSubmitted: _findGroups,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelText: "Группа",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          SizedBox(
-            //TODO задать относительный размер
-            height: 240.0,
+          ];
+        },
+        body: Container(
+          color: Color.fromRGBO(16, 93, 59, 1.0),
+          child: ScrollConfiguration(
+            behavior: MyBehavior(),
             child: ListView(
-              children: results,
+              physics: const NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                Padding(
+                  padding:
+                  const EdgeInsets.only(top: 60.0, right: 30.0, left: 30.0),
+                  child: new TextField(
+                    onSubmitted: _findGroups,
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Группа",
+                      border: OutlineInputBorder(
+                          borderSide: new BorderSide(
+                              color: Color.fromRGBO(16, 93, 59, 1.0))),
+                    ),
+                  ),
+                ),
+                (results.toString() != "[]")
+                    ? Column(children: results)
+                    : Container(
+                  width: 0.0,
+                  height: 0.0,
+                ),
+              ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
